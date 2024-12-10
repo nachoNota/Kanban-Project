d9716785301a7469fba9ccd9482e440f153c84ad
nachoNota <nachonota13@gmail.com>
2024-12-09 22:41:41 -0300

    inicio un nuevo proyecto mvc

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,8 +1,9 @@
-namespace tl2_proyecto_2024_nachoNota.Models;
-
-public class ErrorViewModel
+namespace tl2_proyecto_2024_nachoNota.Models
 {
-    public string? RequestId { get; set; }
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
 }
