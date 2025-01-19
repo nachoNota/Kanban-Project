fef7ba6ccbd46ba3caab9782508c4e77d5db5500
nachoNota <nachonota13@gmail.com>
2025-01-19 18:09:13 -0300

    -

--- Program.cs
+++ Program.cs
@@ -47,7 +47,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Login}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
