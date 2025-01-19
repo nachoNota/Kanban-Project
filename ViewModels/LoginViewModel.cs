4783921482666ac839b7a1ed914c8074d7940317
nachoNota <nachonota13@gmail.com>
2025-01-19 17:39:44 -0300

    hago el controlador para el login

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace tl2_proyecto_2024_nachoNota.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string nombreUsuario { get; set; }
+        public string contrasenia { get; set; }
+        public bool IsAuthenticated {  get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
